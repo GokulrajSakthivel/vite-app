bbb33ca92697a904ed5dd3b7e4631cd8336a87c7
GokulrajBartonMalow <Gokulraj.Sakthivel@bartonmalow.com>
2025-12-22 17:23:23 +0530

    qa moving

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,16 @@ import App from './App.tsx';
 import { Amplify } from 'aws-amplify';
 import awsExports from "./aws-exports";
 
-Amplify.configure(awsExports);
+Amplify.configure({
+  ...awsExports,
+  API:{
+    GraphQL: {
+      endpoint: import.meta.env.VITE_AMPLIFY_GRAPHQL_ENDPOINT,
+      region: import.meta.env.VITE_AMPLIFY_REGION,
+      defaultAuthMode: "userPool",
+    },
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <App />
